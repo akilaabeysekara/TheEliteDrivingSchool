3309c8e7ee8b322b19afc696d971940291d2f9a1
akilaabeysekara <akila2024e@gmail.com>
2025-09-29 15:44:53 +0530

    finalize

---
+++ src/main/java/lk/ijse/elite/bo/exception/IllegalStateException.java
@@ -0,0 +1,7 @@
+package lk.ijse.elite.bo.exception;
+
+public class IllegalStateException extends RuntimeException {
+    public IllegalStateException(String message) {
+        super(message);
+    }
+}
