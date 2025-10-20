3a87450c0d6e0c8809347aa07c4641a09f97e5ef
akilaabeysekara <akila2024e@gmail.com>
2025-10-20 07:38:32 +0530

    re-finalize with replacing already saved program

--- src/main/java/lk/ijse/elite/AppInitializer.java
+++ src/main/java/lk/ijse/elite/AppInitializer.java
@@ -12,14 +12,15 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("/view/LoginPage.fxml"));
-        Parent root = fxmlLoader.load();
+        Parent root =
+                fxmlLoader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Elite Driving School");
         stage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
+
 }
